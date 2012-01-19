0c2721eb1858e3520cdcae0032ddfa350095de6d
Louis DeJardin <louis.dejardin@gmail.com>
2012-01-18 17:50:01 -0800

    Adding extremely simple HelloWorld sample

---
+++ src/main/HelloWorld/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Dragonfly.Http;
+using Gate.Owin;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new ServerFactory();
+            using (server.Create(App, 8080))
+            {
+                Console.WriteLine("Running server on http://localhost:8008/");
+                Console.WriteLine("Press enter to exit");
+                Console.ReadLine();
+            }
+        }
+
+        private static void App(IDictionary<string, object> env, ResultDelegate result, Action<Exception> fault)
+        {
+            result(
+                "200 OK",
+                new Dictionary<string, IEnumerable<string>>(StringComparer.OrdinalIgnoreCase)
+                    {
+                        {"Content-Type", new[] {"text/plain"}}
+                    },
+                (write, error, end) =>
+                    {
+                        var bytes = Encoding.Default.GetBytes("Hello world!");
+                        write(new ArraySegment<byte>(bytes), null);
+                        end();
+                        return () => { };
+                    });
+        }
+    }
+}
---
+++ src/main/HelloWorld/Workaround.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Gate.Owin;
+
+namespace HelloWorld
+{
+    static class Workaround
+    {
+#pragma warning disable 169
+        private static Action _1;
+        private static Func<int, int, int> _2;
+#pragma warning restore 169
+    }
+}
